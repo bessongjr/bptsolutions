c9b218deedf3b880ea747f39608833b52feddbba
Pascal Bessong <164766784+bessongjr@users.noreply.github.com>
2025-12-22 23:17:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Smooth Scroll of Navigation
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
